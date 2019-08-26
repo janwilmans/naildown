2ce38e23c9147194dbd0bdb391377ae2d216a151
Jan Wilmans <janwilmans@gmail.com>
2019-08-26 21:18:08 +0200

    added example of an admin menu

--- naildown-plugin.php
+++ naildown-plugin.php
@@ -58,6 +58,31 @@ add_shortcode('naildown', 'embed_markdown');
 // https://github.com/janwilmans/depcharter/blob/master/README.md
 // https://github.com/janwilmans/depcharter/raw/master/README.md
 
+
+/******************** admin menu ***************************************************************************************/
+
+/** Step 2 (from text above). */
+// disabled for now
+//add_action( 'admin_menu', 'my_plugin_menu' );
+
+/** Step 1. */
+function my_plugin_menu() {
+	add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+}
+
+/** Step 3. */
+function my_plugin_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	echo '<div class="wrap">';
+	echo '<p>Here is where the form would go if I actually had options.</p>';
+	echo '</div>';
+}
+
+/******************** admin menu ***************************************************************************************/
+
+
 /*
 use DOMDocument;
 $doc = new DOMDocument();
